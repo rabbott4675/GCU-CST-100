6a6f96e00db05239b002fb0f2e7b2af41bbebc13
Ryan Abbott <ryanabbott@Ryans-MacBook-Pro.local>
2016-08-22 22:25:26 -0700

    Updates

--- WeekTwo/src/chapterNine/NineTwo.java
+++ WeekTwo/src/chapterNine/NineTwo.java
@@ -7,8 +7,8 @@ public class NineTwo {
 	  public static void main(String[] args) {
 
 	        Stock stock1 = new Stock("ORCL", "Oracle Corporation");
-	        stock1.setCurrentPrice(34.5);
-	        stock1.setCurrentPrice(34.35);
+	        stock1.setCurrentPrice(43.35);
+	        stock1.setCurrentPrice(43.35);
 	        System.out.println("Stock name: " + stock1.getName() + " Symbol: " + stock1.getSymbol());
 	        System.out.println("previous price: " + stock1.getPreviousClosingPrice());
 	        System.out.println("current price: " + stock1.getCurrentPrice());
