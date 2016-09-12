112cf2f102b33be0defc80d023a6d72ef4e882b7
Ryan Abbott <rabbott.abbott@gmail.com>
2016-09-11 21:47:54 -0700

    Milestone Assignment 6

--- FinalProject/src/NFLPlayer.java
+++ FinalProject/src/NFLPlayer.java
@@ -1,37 +1,82 @@
-
-public abstract class NFLPLayer {
+public abstract class NFLPlayer {
 	
-	PlayerManager PM = new PlayerManager();
-		
-		public String firstName =  PM.getFirstName();
-		public String lastName = PM.getLastName();
-		public String team = PM.getLastName();
-		public int age = PM.getAge();
-		public int weight = PM.getWeight();
-		public int heightFeet = PM.getHeightFeet();
-		public int heightInch = PM.getHeightInch();
-		
+	//Define the player stats fields
+	private String firstName;
+	private String lastName;
+	private String team;
+	private int age;
+	private int weight;
+	private int heightFeet;
+	private int heightInch;
+	
+	//Getter and Setter
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getHeightFeet() {
+		return heightFeet;
+	}
+
+	public void setHeightFeet(int heightFeet) {
+		this.heightFeet = heightFeet;
+	}
+
+	public int getHeightInch() {
+		return heightInch;
+	}
+
+	public void setHeightInch(int heightInch) {
+		this.heightInch = heightInch;
+	}
 	
 	//Method for player class, prints players stats
-	public void NFLPlayer(String string, String string2, String string3, int i, int j, int k, int e) {
-		firstName = string;
-		lastName = string2;
-		team = string3;
-		age = i; 
-		weight = j;
-		heightFeet = k; 
-		heightInch = e;
+	public NFLPlayer(String firstName, String lastName, String team, int age, int weight, int heightFeet, int heightInch) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.team = team;
+		this.age = age; 
+		this.weight = weight;
+		this.heightFeet = heightFeet; 
+		this.heightInch = heightInch;
 		
 		
-		//print our individual player stats
-		System.out.printf(""
-				+ "Name: %s %s     "
-				+ "Team: %s      "
-				+ "Age: %d      "
-				+ "Weight: %d      "
-				+ "Height: %d ft. %d in.     \n", 
-				firstName, lastName, team, age, weight, heightFeet, heightInch);
-		}
-	
+			}
 
 }
--- FinalProject/src/OffensivePlayer.java
+++ FinalProject/src/OffensivePlayer.java
@@ -1,8 +1,40 @@
+public class OffensivePlayer extends NFLPlayer  {
 
-public abstract class OffensivePlayer extends NFLPLayer  {
+    private int numOfBlocks;
+    private int yards;
 
-	public static void CreatePlayers(){
-	
-	
+    public OffensivePlayer(String firstName, String lastName, String team, int age, int weight, int heightFeet, int heightInch, int numOfBlocks, int yards) {
+        super(firstName, lastName, team, age, weight, heightFeet, heightInch);
+        this.numOfBlocks = numOfBlocks;
+        this.yards = yards;
+       
+        //print our individual player stats
+		System.out.printf(""
+				+ "Name: %s %s     "
+				+ "Team: %s      "
+				+ "Age: %d      "
+				+ "Weight: %d      "
+				+ "Height: %d ft. %d in.     " 
+				+ "NumOfBlocks : %d   " 
+				+ "Yards : %d \n",
+				firstName, lastName, team, age, weight, heightFeet, heightInch, numOfBlocks, yards);
+    }
+    
+    //getters and setters
+    public int getNumOfBlocks() {
+        return this.numOfBlocks;
+    }
+
+    public void setNumOfBlocks(int numOfBlocks) {
+      this.numOfBlocks = numOfBlocks;
+    } 
+    
+	public int getYards() {
+		return this.yards;
 	}
+
+	public void setYards(int yards) {
+		this.yards = yards;
+	}
+        
 }
