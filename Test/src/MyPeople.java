ce699614b71f7ae7733020a41cf13616057b63d7
Ryan Abbott <rabbott.abbott@gmail.com>
2016-09-17 23:05:59 -0700

    Created Celebrate interface

--- Test/src/MyPeople.java
+++ Test/src/MyPeople.java
@@ -1,65 +1,19 @@
-import java.util.ArrayList;
+import java.util.Random;
 
 public class MyPeople {
-	public static void main(String[] args) {
+	
+		String [] celebratingMessages = {" is jumpung for joy!",
+				" running a marrathon out of excitment!",
+				" is excesivly celebrating!",
+				" is very thankful!"};
 
-        ArrayList<Person> people = new ArrayList<Person>();
-        Person tempPerson = new Person();
-        String firstName;
-        String lastName;
-        char choice = 'y';
-        int count = 1;
+		public String message(){
+			Random random = new Random();
+			int select = random.nextInt(celebratingMessages.length);
+			return celebratingMessages[select];
+		}
 
-        // fill my ArrayList
-        do {
-            people.add(tempPerson);
-          
-
-            tempPerson.setFirstName(firstName);
-            tempPerson.setLastName(lastName);
-
-            count++;
-
-            choice = Util.prompt(
-                    "Enter another person? [y or n]: ")
-                    .toLowerCase().charAt(0);
-
-        } while (choice == 'y');
-
-        // display my list of people
-        for(int i = 0; i < people.size(); i += 1) {
-            System.out.print(people.get(i));
-        }
-
-    }
-
-}
-
-// my Person class which I am trying to build from
-public class Person {
-
-    // instance variables
-    private String firstName;
-    private String lastName;
-
-    // default constructor
-    public Person() {
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-}
+		public void main (String [] args) {
+			System.out.println(message());
+		}
 }
